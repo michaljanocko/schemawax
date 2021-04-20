8940e289b675a0d1236f87d5ac6e791f966e964d
Michal Janočko <michal@janocko.com>
2021-04-20 16:42:26 -0500

    :construction: Trying to simplify the code

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
-interface Decoder<A> {
-  readonly decode: (data: unknown) => A
+interface Decoder<D> {
+  readonly decode: (data: unknown) => D
 }
 
 class DecoderError extends SyntaxError {
-  constructor (data: unknown, type: string) {
-    super(`This is not ${type}: ${JSON.stringify(data, null, 2)}`)
+  constructor (error: 'missing' | 'wrongType', data: unknown, type: string) {
+    if (error === 'missing') {
+      super(`Property of type ${type} is missing`)
+    } else {
+      super(`This is not ${type}: ${JSON.stringify(data, null, 2)}`)
+    }
   }
 }
 
@@ -12,62 +16,74 @@ export const unknown: Decoder<unknown> = {
   decode: (data) => data
 }
 
-export const string: Decoder<string> = {
+const decoder = <D>(condition: (data: unknown) => data is D, parser: (data: D) => D, type: string): Decoder<D> => ({
   decode: (data) => {
-    if (typeof data === 'string') {
-      return data
-    } else {
-      throw new DecoderError(data, 'a string')
+    if (data === null || data === undefined) {
+      throw new DecoderError('missing', data, type)
     }
-  }
-}
 
-export const number: Decoder<number> = {
-  decode: (data) => {
-    if (typeof data === 'number') {
-      return data
-    } else {
-      throw new DecoderError(data, 'a number')
+    if (!condition(data)) {
+      throw new DecoderError('wrongType', data, type)
     }
-  }
-}
 
-export const boolean: Decoder<boolean> = {
-  decode: (data) => {
-    if (typeof data === 'boolean') {
-      return data
-    } else {
-      throw new DecoderError(data, 'a boolean')
-    }
-  }
-}
-
-export const literal = (types: unknown[]): Decoder<unknown> => ({
-  decode: (data) => {
-    if (types.some($ => $ === data)) {
-      return data
-    } else {
-      throw new DecoderError(data, `in [${types.map($ => JSON.stringify($)).join(' | ')}]`)
-    }
+    return parser(data)
   }
 })
 
-export const nullable = <A>(decoder: Decoder<A>): Decoder<null | A> => ({
-  decode: (data) => {
-    if (data === null) {
-      return null
-    } else {
-      return decoder.decode(data)
-    }
-  }
-})
+export const string = decoder<string>(
+  ($): $ is string => typeof $ === 'string',
+  ($) => $,
+  'a string'
+)
 
-export const array = <A>(decoder: Decoder<A>): Decoder<A[]> => ({
-  decode: (data) => {
-    if (Array.isArray(data)) {
-      return data.map($ => decoder.decode($))
-    } else {
-      throw new DecoderError(data, 'an array')
-    }
-  }
-})
+export const number = decoder<number>(
+  ($): $ is number => typeof $ === 'number',
+  ($) => $,
+  'a number'
+)
+
+export const boolean = decoder<boolean>(
+  ($): $ is boolean => typeof $ === 'boolean',
+  ($) => $,
+  'a boolean'
+)
+
+// export const literal = (types: unknown[]): Decoder<unknown> => ({
+//   decode: (data) => {
+//     if (types.some($ => $ === data)) {
+//       return data
+//     } else {
+//       throw new DecoderError(data, `in [${types.map($ => JSON.stringify($)).join(' | ')}]`)
+//     }
+//   }
+// })
+
+// export const nullable = <A>(decoder: Decoder<A>): Decoder<null | A> => ({
+//   decode: (data) => {
+//     if (data === null) {
+//       return null
+//     } else {
+//       return decoder.decode(data)
+//     }
+//   }
+// })
+
+// export const array = <A>(decoder: Decoder<A>): Decoder<A[]> => ({
+//   decode: (data) => {
+//     if (Array.isArray(data)) {
+//       return data.map($ => decoder.decode($))
+//     } else {
+//       throw new DecoderError(data, 'an array')
+//     }
+//   }
+// })
+
+// export const record = <A>(decoder: Decoder<A>): Decoder<Record<string, A>> => ({
+//   decode: (data) => {
+//     if (typeof data === 'object') {
+//       return Object.entries(data).map([key, value] => )
+//     } else {
+//       throw new DecoderError(data, 'an object')
+//     }
+//   }
+// })
